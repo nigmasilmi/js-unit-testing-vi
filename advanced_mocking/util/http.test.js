5fbc70aa189257f450d2b73e951de41206a17bde
Nigma Silmi <sn.silmi@gmail.com>
2022-07-01 12:28:01 -0400

    testing the options/body in a fetch request

--- advanced_mocking/util/http.test.js
+++ advanced_mocking/util/http.test.js
@@ -10,6 +10,11 @@ const testFetch = vi.fn((url, options) => {
   return new Promise((resolve, reject) => {
     // because the fetch response looks like this
     // and we are using response.ok in our implementation
+
+    // to test if the provided data in the body is a JSON
+    if (typeof options.body !== "string") {
+      return reject("not a string");
+    }
     const testResponse = {
       ok: true,
       // because this method returns a promise
@@ -29,4 +34,18 @@ vi.stubGlobal("fetch", testFetch);
 it("should return any available response data", () => {
   const testData = { key: "test" };
   return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+});
+
+it("should convert the provided data to JSON before sending the request", async () => {
+  const testData = { key: "test" };
+  // this test expects a rejection
+  //expect(sendDataRequest(testData)).not.rejects.toBe("not a string");
+  // a better way to test it is:
+  let errorMessage;
+  try {
+    await sendDataRequest(testData);
+  } catch (err) {
+    errorMessage = err;
+  }
+  expect(errorMessage).not.toBe("not a string");
 });
